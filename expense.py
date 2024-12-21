b9093bc08f2f9b26837f4defa3a6202600997f06
sairandhree <sairandhree.sule@gmail.com>
2024-12-21 23:36:51 +0530

    functional

--- expense.py
+++ expense.py
@@ -1,11 +1,13 @@
 class Expense:
-    def __init__(self, name, category, amount):
-        self.name = name
+    def __init__(self, desc, category, amount, date):
+        self.desc = desc
         self.category = category
         self.amount = amount
+        self.date = date
+
     
     # def __str__(self):
     #     return "<Expense: {self.name}, {self.category}, ${self.amount:.2f} >"
     def __repr__(self):
-         return "{self.name}, {self.category}, ${self.amount:.2f}"
+         return f"{{'date': {self.date}, 'category': {self.category}, 'amount':{self.amount}, 'description':{self.desc} }}"
     
--- expense_tracker.py
+++ expense_tracker.py
@@ -1,17 +1,43 @@
+from dateutil.utils import today
+
 from expense import Expense
 import calendar
-import datetime
+from datetime import date
 
+budget = 0
 
 def main():
     print ("Running Expense Tracker!")
     expense_file_path = "expenses.csv"
-    budget = 2000
-    expense = get_user_expense()
-    print(expense)
-    save_expense_to_file(expense, expense_file_path)
+   
+    expense = None
+    while True:
+        print("Welcome to the Expense Tracker!")
+        print("Please choose an option:")
+        print("1. Add an expense")
+        print("2. View expenses")
+        print("3. Track budget")
+        print("4. Save expenses")
+        print("5. Exit")
+        user_choice = int(input("Enter your choice: "))
+        if user_choice == 1:
+            expense = get_user_expense()
+            print(f"you have entered {expense}")
+        elif user_choice == 2:
+            read_expenses_from_file(expense_file_path)
+        elif user_choice == 3:
+            track_budget(expense_file_path)
+        elif user_choice == 4:
+            save_expense_to_file(expense,expense_file_path)
+        elif user_choice == 5:
+            print("Exiting Expense Tracker. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again!")
 
-    read_expenses_from_file(expense_file_path)
+
+            
+
 
 def get_user_expense():
     expense_name = input("Enter expense name: ")
@@ -36,35 +62,54 @@ def get_user_expense():
                 selected_category = expense_categories[selected_index]
                 print("you choose category: " + selected_category)
                 
-                new_expense = Expense(
-                     name=expense_name, category=selected_category, amount=amount
-                 )
+                new_expense = Expense(desc=expense_name, category=selected_category, amount=amount, date=date.today())
                 return new_expense
             else:
                 print("Invalid category. Please try again!")
 
 def save_expense_to_file(expense, file_path):
     with open(file_path, "a") as file:
-         file.write(f"{expense.name},{expense.amount},{expense.category}\n")
+         file.write(f"{{'date': '{expense.date}', 'category': '{expense.category}', 'amount':{expense.amount}, 'description':'{expense.desc}' }}" + "\n")
     print(f"Expense saved to {file_path} successfully!")
 
 def read_expenses_from_file(file_path):
 
-    list_of_expenses = []
-    total_expenses = 0
+    list_of_expenses = []    
     with open(file_path, "r") as file:
         lines = file.readlines()
         for line in lines:
-            name, amount, category = line.strip().split(",")
-            expense = Expense(name=name, amount=amount, category=category)
-            total_expenses += int(expense.amount)
+            expense_dict = eval(line)
+            expense = Expense(
+                desc=expense_dict['description'],
+                category=expense_dict['category'],
+                amount=expense_dict['amount'],
+                date=expense_dict['date']
+            )
+          
             list_of_expenses.append(expense)
 
     print("Expenses read from file: ")
     for expense in list_of_expenses:
         print(expense)
 
-    print(f"Total expenses: ${total_expenses:.2f}")
+ 
+
+def track_budget( file_path):
+    global budget
+    if budget == 0:
+        budget = float(input("Enter your budget: "))
+
+    total_expenses = 0
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            expense_dict = eval(line)            
+            total_expenses += float(expense_dict['amount'])
+
+    if total_expenses > budget:
+        print("You have exceeded your budget!\n\n")
+    else:
+        print(f"You have {budget-total_expenses}  left in your budget! \n\n")
 
 
 
